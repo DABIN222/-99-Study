13b87b86a977b5f394ea0938c948055bc9bc44d4
KDBjava <96757077+DABIN222@users.noreply.github.com>
2022-01-17 10:53:09 +0900

    Create BOJ_1271.java

---
+++ BOJ_1271.java
@@ -0,0 +1,23 @@
+package com.our.study;
+
+import java.math.BigInteger;
+
+
+public class Study01_Q1271 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BigInteger n = new BigInteger("1000");
+		BigInteger m = new BigInteger("100");
+	
+
+		
+		System.out.println(n.divide(m));
+	
+		System.out.println(n.remainder(m));
+		
+		
+	}
+
+}
